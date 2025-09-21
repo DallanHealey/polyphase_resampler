b6519e35a537a19585d18a16030ffd3300932795
Dallan Healey <dallan.healey@live.com>
2025-09-20 21:05:27 -0400

    feature/systemverilog: Changed to use surfer as my fst viewer

--- verilator/sim.sh
+++ verilator/sim.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# This script assumes you have verilator installed and on your PATH
+# https://github.com/verilator/verilator
+
+# This script assumes you have surfer installed and on your PATH
+# https://gitlab.com/surfer-project/surfer
+
 verilator --trace-max-array 128 -f sim.f
 ret=$?
 
 if [ $ret -eq 0 ]; then
     obj_dir/Vtb_resampler
-    gtkwave vsim.fst
+    surfer -s waveform_view vsim.fst
 else
     echo "Errors found."
 fi
