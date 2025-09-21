4d27f737b705f1355b32d43b17b2b9a168f69967
Dallan Healey <dallan.healey@live.com>
2025-09-20 21:14:10 -0400

    feature/systemverilog: Printing out the correct values of the easiest possible upsampler

--- resampler_up3_down2.sv
+++ resampler_up3_down2.sv
@@ -20,18 +20,41 @@ localparam integer NUM_COLS = NUM_TAPS / UP;
 
 logic [NUM_COLS-1:0][15:0] sample_i_sr;
 logic [NUM_COLS-1:0][15:0] sample_q_sr;
+logic                      sample_sr_valid;
 
+logic [UP-1:0][31+NUM_COLS:0] current_phase;
+logic [UP-1:0][31+NUM_COLS:0] next_current_phase;
+
+// Shift data in
 always @(posedge clk_1x) begin
+    sample_sr_valid <= sample_valid_i;
+
     if (sample_valid_i == 1'b1) begin
         if (NUM_COLS == 1) begin
             sample_i_sr <= sample_i_i;
             sample_q_sr <= sample_q_i;
+
+            current_phase <= next_current_phase;
         end else begin
             sample_i_sr <= {sample_i_sr[NUM_COLS-2:0], sample_i_i};
             sample_q_sr <= {sample_q_sr[NUM_COLS-2:0], sample_q_i};
+
+            current_phase <= next_current_phase;
         end
     end
 end
 
+always_comb begin
+    next_current_phase = current_phase;
+
+    for (integer i = 0; i < NUM_COLS; i++) begin
+        if (sample_sr_valid == 1'b1) begin
+            for (integer j = 0; j < UP; j++) begin
+                next_current_phase[j] = sample_i_sr[i] * TAPS[i*UP+j];
+                $display("%h %h %h", sample_i_sr[i], TAPS[i*UP+j], next_current_phase[j]);
+            end
+        end
+    end
+end
 
 endmodule
--- verilator/tb_resampler.sv
+++ verilator/tb_resampler.sv
@@ -11,6 +11,9 @@ logic [15:0] sample_i_o;
 logic [15:0] sample_q_o;
 logic        sample_valid_o;
 
+logic start;
+event samples_done;
+
 localparam CLK_1X_PERIOD = 10;
 always begin
     #(CLK_1X_PERIOD/2);
@@ -23,18 +26,52 @@ always begin
     clk_1p5x <= !clk_1p5x;
 end
 
+localparam integer NUM_SAMPLES = 2;
+logic [0:NUM_SAMPLES-1][15:0] samples = {16'd1, 16'd2};
+localparam integer NUM_TAPS = 3;
+localparam logic [0:NUM_TAPS-1][15:0] taps = {16'd3, 16'd4, 16'd3};
+
 initial begin
     $dumpfile("vsim.fst");
     $dumpvars(0, tb_resampler);
 
     clk_1x = 1'b0;
     clk_1p5x = 1'b0;
+    start = 1'b0;
+    #10us;
 
+    // Start sending samples
+    $display("%T: Starting to send samples...", $time);
+    start = 1'b1;
+
+    // Wait for samples to finish sending
+    @(samples_done);
+    start = 1'b0;
+    $display("%T: Done sending samples...", $time);
     #10us;
     $finish;
 end
 
-resampler_up3_down2 DUT (
+integer sample_count;
+always @(posedge clk_1x) begin
+    sample_valid_i <= 1'b0;
+
+    if (start == 1'b1) begin
+        sample_valid_i <= 1'b1;
+        sample_i_i <= samples[sample_count];
+        if (sample_count == NUM_SAMPLES - 1) begin
+            sample_count <= 0;
+            ->samples_done;
+        end else begin
+            sample_count <= sample_count + 1;
+        end
+    end
+end
+
+resampler_up3_down2 # (
+    .NUM_TAPS(NUM_TAPS),
+    .TAPS(taps)
+) DUT (
     .*
 );
 
