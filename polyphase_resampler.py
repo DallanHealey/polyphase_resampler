85d0eea7fc702935034faff89718b6d15f933720
Dallan Healey <dallan.healey@live.com>
2025-09-20 21:04:45 -0400

    feature/systemverilog: Added a debug mode to the resampler so I can print known values to make proving my SV easier

--- polyphase_resampler.py
+++ polyphase_resampler.py
@@ -2,6 +2,16 @@ import numpy as np
 import scipy.signal as sig
 import matplotlib.pyplot as plt
 from functools import reduce
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog = "polyphase_resampler",
+    description = "This is a test program that helps prove out my arbitrary polyphase resampler code",
+    epilog = ""
+)
+
+parser.add_argument("-d", "--debug", action="store_true", help="Runs with chosen parameters instead of random checks")
+args = parser.parse_args()
 
 # Found on StackOverflow: https://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python/
 def factors(n):
@@ -9,26 +19,25 @@ def factors(n):
         list.__add__,
         ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
-# iq_file = open("test_file.iq", "r")
-# samples_i = []
-# samples_q = []
-# for line in iq_file:
-#     l = line.strip().split(" ")
-#     for s in l:
-#         samples_i.append(int.from_bytes(bytes.fromhex(s[0:4]), signed=True, byteorder="big"))
-#         samples_q.append(int.from_bytes(bytes.fromhex(s[4: ]), signed=True, byteorder="big"))
-# samples_i = np.array(samples_i).astype(np.int16)
-# samples_q = np.array(samples_q).astype(np.int16)
-# samples_iq = samples_i + samples_q*1j
+# Debug print only if DEBUG is True
+def dprint(*text):
+    if (args.debug == True):
+        print(*text)
 
-# print(samples_iq)
+if (args.debug == True):
+    data = [1, 2]
+    taps = [3, 4, 3]
+    num_taps = len(taps)
+    up = 3
+    down =2
+else:
+    data = [np.random.randint(-2**16, 2**16)]*np.random.randint(low=1, high=10)
+    num_taps = np.random.randint(1, 10)
+    taps = sig.firwin(num_taps, 20, fs=80).astype(np.int32)
+    fact = list(factors(len(taps)))
+    up = fact[np.random.randint(0, len(fact))]
+    down = np.random.randint(1, 10)
 
-data = [np.random.randint(-2**16, 2**16)]*np.random.randint(low=1, high=10)
-num_taps = np.random.randint(1, 10)
-taps = sig.firwin(num_taps, 20, fs=80).astype(np.int32)
-fact = list(factors(len(taps)))
-up = fact[np.random.randint(0, len(fact))]
-down = np.random.randint(1, 10)
 num_taps_col = int(len(taps) / up)
 
 assert len(taps) % up == 0, "Taps aren't a multiple of the up conversion rate"
@@ -46,7 +55,7 @@ for i in range(0, len(data)+num_taps_col-1):
     for m in range(0, up):
         for j in range(0, num_taps_col):
             current_phase[m] += current_sample_list[j]*taps[j*up+m]
-
+        dprint(m, current_phase[m])
         if (k == 0 or down == 1):
             upsamples.append(current_phase[m])
             k += 1
@@ -56,6 +65,8 @@ for i in range(0, len(data)+num_taps_col-1):
             k += 1
 
 sci_samples = sig.upfirdn(taps, data, up, down)
+dprint(sci_samples)
+dprint(upsamples)
 
 if ((sci_samples == upsamples).all()):
     print("Correct")
